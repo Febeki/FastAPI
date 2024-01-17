b69bfb44e81450b074955a243b6229ac8246bb66
Denis Gudkov <gudkov.den.5454@gmail.com>
2024-01-17 23:16:31 +0500

    jinja + websocket + static + js

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
 from auth.base_config import auth_backend, fastapi_users
 from auth.schemas import UserRead, UserCreate
 
 from operations.router import router as router_operation
 from tasks.router import router as router_tasks
+from pages.router import router as router_pages
+from chat.router import router as router_chat
 
 app = FastAPI(
     title="Trading App"
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
@@ -24,6 +30,8 @@ app.include_router(
 
 app.include_router(router_operation)
 app.include_router(router_tasks)
+app.include_router(router_pages)
+app.include_router(router_chat)
 
 origins = [
     "http://localhost:3000",
